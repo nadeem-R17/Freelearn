b8aefb28d1fc5864336f441a1a4473a3ad6c637c
Nadeem Raza <text.12mail@gmail.com>
2021-12-31 20:55:58 +0530

    urls changed

--- beta/urls.py
+++ beta/urls.py
@@ -18,6 +18,7 @@ from django.urls import path, include
 
 from django.views.static import serve
 from django.conf.urls import url 
+from django.conf.urls.static import static 
 from django.conf import settings
 
 urlpatterns = [
@@ -28,3 +29,4 @@ urlpatterns = [
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
         
 ]
+urlpatterns=urlpatterns+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
